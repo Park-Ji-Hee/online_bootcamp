fbc8c3e76dc9b9455625d28783179045b4c6b434
jihee <qkrwlgml403@gmail.com>
2025-01-19 15:07:07 +0900

    8일차 커밋

---
+++ practice/javascript.js
@@ -0,0 +1,26 @@
+altert();                   -> 알림창
+
+prompt();                   -> 입력창
+
+var 변수 = ;                 -> 변수 선언
+
+변수.length;                 -> 변수 길이이
+
+.slice(시작글자, 끝나는 위치); -> 글자 자르기
+    var name = "Jihee";
+    name.slice(0,1);  ==> J
+
+.toUpperCase();              -> 모두 대문자로 바꿈
+
+function 변수() {};          -> {}안에 들어간 기능 시행
+
+console.leg();               -> 출력
+
+Math.random();               -> 숫자랜덤
+
+
+
+
+
+
+
